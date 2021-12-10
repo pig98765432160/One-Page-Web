078278b266c1e2d2cb14c27fc6a72fa2c282f3f0
Vivian wu <pig98765432160@gmail.com>
2021-12-10 10:35:22 +0800

    [加入] 一頁式網頁 & json
    新增圖片、刪除原生css&js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Page extends React.Component {
+    render() {
+        return (
+            <>
+                <img className='titleImg' src="./img/photo1.jpg" alt="photo1" />
+                <div>
+                    <p>PRODUCTS & SERVICES</p>
+                    <p>INSURANCE COVERAGE</p>
+                    <div className='blocks'>
+                        <div className='block'>
+                            <img className='img' src="./img/img0.jpg" alt="img0" />
+                            <p>AUTO INSURANCE</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, voluptate.</p>
+                            <button>LEARN MORE</button>
+                        </div>
+                        <div className='block'>
+                            <img className='img' src="./img/img0.jpg" alt="img0" />
+                            <p>AUTO INSURANCE</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, voluptate.</p>
+                            <button>LEARN MORE</button>
+                        </div>
+                        <div className='block'>
+                            <img className='img' src="./img/img0.jpg" alt="img0" />
+                            <p>AUTO INSURANCE</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, voluptate.</p>
+                            <button>LEARN MORE</button>
+                        </div>
+                    </div>
+                    <div>
+                        <p>WE ARE TRUSTED AGENCY COMPANY</p>
+                        <img className='photo' src="./img/img1.jpg" alt="img1" />
+                        <div className='blocks'>
+                            <p className='intro'>aaaa</p>
+                            <p className='intro'>bbbb</p>
+                            <p className='intro'>cccc</p>
+                        </div>
+                    </div>
+                    <div className ='field'>
+                        <img className='photo' src="./img/img1.jpg" alt="img1" />
+                        <div>
+                            <p>OUTSTANDING SERVICES</p>
+                            <p>WHY CHOOSE US</p>
+                            <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+                        </div>
+                    </div>
+                    <div className ='field'>
+                        <div>
+                            <p>OUTSTANDING SERVICES</p>
+                            <p>WHY CHOOSE US</p>
+                            <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+                        </div>
+                        <img className='photo' src="./img/img1.jpg" alt="img1" />
+                    </div>
+                    <div>
+                        <p>OUTSTANDING SERVICES</p>
+                        <p>WHY CHOOSE US</p>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Page />,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
